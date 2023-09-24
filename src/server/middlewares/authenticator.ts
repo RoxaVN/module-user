da04fbd93cddaa2125ec647a75ca605d989de47f
Toan Nguyen <woodpecker146@gmail.com>
2023-09-24 14:10:00 +0700

    chore: implement CheckAccessTokenService

--- src/server/middlewares/authenticator.ts
+++ src/server/middlewares/authenticator.ts
@@ -1,20 +1,16 @@
 import {
-  ApiAuthenticatorMiddleware,
-  BaseService,
+  CheckAccessTokenService,
   DatabaseService,
-  LoaderAuthenticatorMiddleware,
-  MiddlewareService,
-  RouterContext,
   inject,
 } from '@roxavn/core/server';
-import { Cookie, constants } from '@roxavn/core/base';
+import { TokenService } from '@roxavn/module-utils/server';
+
 import { Raw } from 'typeorm';
 import { AccessToken } from '../entities/index.js';
 import { serverModule } from '../module.js';
-import { TokenService } from '@roxavn/module-utils/server';
 
-@serverModule.injectable()
-export class CheckTokenService extends BaseService {
+@serverModule.rebind(CheckAccessTokenService)
+export class CheckAccessTokenServiceEx extends CheckAccessTokenService {
   constructor(
     @inject(DatabaseService) protected databaseService: DatabaseService,
     @inject(TokenService) protected tokenService: TokenService
@@ -49,10 +45,7 @@ export class CheckTokenService extends BaseService {
           token: signature,
           expiryDate: Raw((alias) => `${alias} > NOW()`),
         },
-        cache: {
-          id: 'getAccessToken',
-          milliseconds: 60000, // 1 minute
-        },
+        cache: 60000, // 1 minute,
       });
 
     if (!accessToken) {
@@ -63,60 +56,5 @@ export class CheckTokenService extends BaseService {
       user: { id: accessToken.userId },
       accessToken: { id: accessToken.id },
     };
-  }
-}
-
-@serverModule.rebind(ApiAuthenticatorMiddleware)
-export class ApiAuthenticatorMiddlewareEx
-  extends BaseService
-  implements MiddlewareService
-{
-  constructor(
-    @inject(CheckTokenService) protected checkTokenService: CheckTokenService
-  ) {
-    super();
-  }
-
-  async handle(
-    { api, state, request }: RouterContext,
-    next: () => Promise<void>
-  ) {
-    if (api?.permission) {
-      const authorizationHeader = request.headers.get('authorization');
-      const token =
-        authorizationHeader && authorizationHeader.startsWith('Bearer ')
-          ? authorizationHeader.slice(7)
-          : null;
-      const authData = await this.checkTokenService.handle({ token });
-      Object.assign(state, authData);
-    }
-    return next();
-  }
-}
-
-@serverModule.rebind(LoaderAuthenticatorMiddleware)
-export class LoaderAuthenticatorMiddlewareEx
-  extends BaseService
-  implements MiddlewareService
-{
-  constructor(
-    @inject(CheckTokenService) protected checkTokenService: CheckTokenService
-  ) {
-    super();
-  }
-
-  async handle(
-    { api, state, request }: RouterContext,
-    next: () => Promise<void>
-  ) {
-    if (api?.permission) {
-      const cookie = request.headers.get('cookie');
-      const token = cookie
-        ? new Cookie(cookie).get(constants.Cookie.TOKEN)
-        : null;
-      const authData = await this.checkTokenService.handle({ token });
-      Object.assign(state, authData);
-    }
-    return next();
   }
 }
